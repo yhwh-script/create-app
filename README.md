6fdba8973aa0f2ceeba80fea43843bb70851f752
yhwh-script <yhwhscript@gmail.com>
2025-01-29 17:24:16 +0100

    feat: inquirer examples

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Easiest way is to simply call the following command:
 
 ```
 Need to install the following packages:
-@yhwh-script/create-app@0.4.1
+@yhwh-script/create-app@0.4.3
 Ok to proceed? (y) y
 ```
 
--- bin/create-app.mjs
+++ bin/create-app.mjs
@@ -3,7 +3,7 @@ import { argv, chdir, cwd } from "node:process";
 import { join } from "node:path";
 import { existsSync, mkdirSync, opendir, cpSync } from "node:fs";
 import { execSync } from "node:child_process";
-import { checkbox, Separator } from '@inquirer/prompts';
+import { checkbox, Seperator } from "@inquirer/prompts"
 
 const projectname = argv[2];
 
@@ -11,17 +11,15 @@ if (!projectname) {
   throw new Error("Please specify a project name. Syntax: npx @yhwh-script/create-app {your_project_name}");
 }
 
-const answer = await checkbox({
+const answers = await checkbox({
   message: 'Select your packages',
   choices: [
     { name: 'elements', value: 'elements', disabled: '(will be installed)' },
-    new Separator(),
+    new Seperator(),
     { name: 'examples', value: 'examples' },
     { name: 'sqlite', value: 'sqlite' },
   ],
 });
-
-console.log("answer", answer);
 
 const directory = join(cwd(), projectname);
 
@@ -42,8 +40,22 @@ opendir(directory, (err, dir) => {
     execSync(`npm pkg delete scripts.test`);
     execSync(`npm pkg set type=module`);
 
+    let files = 'files';
+    if (answers.includes['sqlite']) {
+      console.log("Installing SQLite...");
+      execSync("npm install @sqlite.org/sqlite-wasm --save");
+      if (answers.includes['examples']) {
+        files = 'sqlite-examples';
+      } else {
+        files = 'sqlite';
+      }
+    } else if (answers.includes['examples']) {
+        files = 'examples';
+      }
+    // kein sqlite, keine examples
+
     console.log("Copying files...");
-    const defaultPath = join(import.meta.dirname, "../files");
+    const defaultPath = join(import.meta.dirname, `../${files}`);
     const targetPath = join(dir.path);
     cpSync(defaultPath, targetPath, { recursive: true });
 
@@ -52,9 +64,6 @@ opendir(directory, (err, dir) => {
     execSync(
       `npm pkg set scripts.gen="node gen.cjs" scripts.dev="npm run gen && vite" scripts.build="npm run gen && vite build" scripts.preview="npm run gen && vite preview"`
     );
-
-    // console.log("Installing SQLite...");
-    // execSync("npm install @sqlite.org/sqlite-wasm --save");
 
     console.log("Closing the directory");
     dir.closeSync();
