11f6092e00d25617af718f38835629a084df172b
J4H20M3 <rome@live.com>
2025-03-12 12:08:26 +0100

    fix: vite build

--- examples/vite.config.js
+++ examples/vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+    build: {
+        target: 'esnext'
+    },
     server: {
         port: 3443,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        },
+    },
+    optimizeDeps: {
+        exclude: ['@sqlite.org/sqlite-wasm'],
     },
     plugins: [
         basicSsl()
